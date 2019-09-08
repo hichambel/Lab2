110cda631c389cba65e40549aaad56c11e583624
Hicham <Hicham@DESKTOP-MLM44N8>
2019-09-08 16:42:35 -0400

    Add project files.

---
+++ Lab2/Controllers/GradesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class GradesController : Controller
+    {
+        // GET: Grades
+        [HttpGet]
+        public ActionResult IndexGrades()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IndexGrades(FormCollection form)
+        {
+            ViewData["letterGrade"] = "";
+            if (form["gradeOne"] == null && form["gradeTwo"] == null && form["gradeThree"] == null) {    
+                    ViewData["letterGrade"] = "You must fill out all the form";
+            }
+            else{
+                double firstGrade = double.Parse(form["gradeOne"]);
+                double secondGrade = double.Parse(form["gradeTwo"]);
+                double thirdGrade = double.Parse(form["gradeThree"]);
+                double average = (firstGrade + secondGrade + thirdGrade) / 3;
+
+                if (average >= 90 && average <= 100) {
+                    ViewData["letterGrade"] = "Your letter grade is A";
+                } else if (average >= 80 && average <= 89) {
+                    ViewData["letterGrade"] = "Your letter grade is B";
+                } else if (average >= 70 && average <= 79) {
+                    ViewData["letterGrade"] = "Your letter grade is C";
+                } else if (average >= 60 && average <= 69) {
+                    ViewData["letterGrade"] = "Your letter grade is D";
+                } else if (average <= 59) {
+                    ViewData["letterGrade"] = "Your letter grade is F";
+                }
+            } 
+            return View();
+        }
+    }
+}
---
+++ Lab2/Controllers/LoanController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class LoanController : Controller
+    {
+        // GET: Loan
+        [HttpGet]
+        public ActionResult IndexLoan()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IndexLoan(double years, double amount, double rate)
+        {
+            ViewData["years"] = years;
+            ViewData["amount"] = amount;
+            ViewData["rate"] = rate;
+            for (int x = 0; x < years; x++) {
+                amount += (rate / 100) * amount;
+            }
+            ViewData["finalAmount"] = Math.Round(amount,2);
+            return View();
+        }
+    }
+}
